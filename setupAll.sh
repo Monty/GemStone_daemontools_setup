0445409fdaa661302787a20d7a8d7d876d33b588
Monty Williams <monty.williams@gemstone.com>
2010-08-05 09:29:54 -0700

    Initial commit. Code mostly works. copySeasideScripts assumes the seaside/bin files are owned by the user running GemStone which is not always the case. I'll fix that later.

---
+++ setupAll.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ $UID -ne 0 ]; then
+   echo "You must be root to run this!"
+   exit
+fi 
+
+# Get params
+source config
+
+echo "This script executes all daemontools setup scripts in the proper seauence."
+echo "Type \"Y\" to confirm each step prior to execution. Type \"N\" to skip that step."
+echo "NOTE: Make sure you have already installed GemStone/S and the the config file"
+echo "in this directory contains appropriate settings:"
+echo "#-------------------------------------------------------"
+cat config
+echo "#-------------------------------------------------------"
+
+echo "Shall I continue? (Y/N)"
+
+read cont
+case "$cont" in
+    y|Y|yes)
+    ;;
+    *)
+    echo "Not continuing"
+    exit
+    ;;
+esac
+
+./copySeasideScripts
+#
+case "$SEASIDE_VERSION" in
+    3.0)
+    ./daemontools_setup30
+    ;;
+    2.8)
+    ./daemontools_setup
+    ;;
+    *)
+    echo "SEASIDE_VERSION in the config file is $SEASIDE_VERSION"
+    echo "it must be either 3.0 or 2.8"
+    ;;
+esac
+#
+./bootGemstone_setup
+#
+./daemontools_start
+#
+./bootGemstone_start
+
+# Success
+exit 0
